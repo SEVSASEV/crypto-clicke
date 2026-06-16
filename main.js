a3c9d88db123b57b1a13aec5652e91d4afe98686
Sevo4ka <156247058+SEVSASEV@users.noreply.github.com>
2026-06-16 06:07:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 480,
+    height: 800,
+    minWidth: 400,
+    minHeight: 650,
+    title: "Крипто Кликер Game",
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:5173');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, 'dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
